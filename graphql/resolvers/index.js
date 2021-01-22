25860eb4fbb96e924ea1bb424ca61e459bf2cc9c
wuqianye <natsumi3613@gmail.com>
2021-01-22 02:51:43 -0500

    init

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,18 @@
+const postsResolvers = require("./posts")
+const usersResolvers = require("./users")
+const commentsResolvers = require("./comments")
+
+module.exports = {
+    Post: {
+        commentCount: (parent) => parent.comments.length,
+        likeCount: (parent) => parent.likes.length
+    },
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    }
+}
